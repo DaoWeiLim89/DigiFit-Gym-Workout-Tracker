43a9083c08811c2091b30ff70956e8a91f6bb0d7
Elliotkim2 <elliotkim0424@gmail.com>
2025-10-31 19:59:34 -0400

    added getUser and setUser

--- src/main/java/com/DigiFit/GymWorkoutTracker/WorkoutSplit.java
+++ src/main/java/com/DigiFit/GymWorkoutTracker/WorkoutSplit.java
@@ -31,6 +31,9 @@ public class WorkoutSplit {
     // ----- Getters & Setters -----
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
+    
+    public User getUser() { return user; }
+    public void setUser(User user) { this.user = user; }
 
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
